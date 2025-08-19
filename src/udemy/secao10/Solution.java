d2b1625414d98393e5d11dc238bcd19993b6ac39
Cauã Bemfica Macedo <146380777+Cauab12@users.noreply.github.com>
2025-08-19 17:11:20 -0300

    Update Solution.java

--- src/udemy/secao10/Solution.java
+++ src/udemy/secao10/Solution.java
@@ -1,6 +1,6 @@
 package udemy.secao10;
 
-class Solution {
+class SolutionLeetCode {
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i <= nums.length - 1; i++) {
             for (int j = i + 1; j <= nums.length - 1; j++) {
